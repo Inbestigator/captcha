d697d092bb991f36912fbb92c301872882d0940d
Inbestigator <inbestigatoryt@gmail.com>
2026-07-23 03:33:20 -0700

    fix(stats): more efficient query

--- src/db/cache.ts
+++ src/db/cache.ts
@@ -1,7 +1,7 @@
 import { hash } from "node:crypto";
 import { createCache } from "@dressed/ws/cache";
 import { createDM, getGuild } from "dressed";
-import { eq } from "drizzle-orm";
+import { count, eq, sum } from "drizzle-orm";
 import { db } from "./db.ts";
 import { settingsTable, stagesTable } from "./schema.ts";
 
@@ -22,16 +22,16 @@ export const startCache = (redis: {
         return res[0] ?? null;
       },
       async getStats() {
-        const failPromise = db.select({ fails: stagesTable.fails }).from(stagesTable);
+        const stagesPromise = db.select({ challenges: count(), fails: sum(stagesTable.fails) }).from(stagesTable);
         const keys = await redis.keys("check:*");
-        const [fails, ...checks] = await Promise.all([
-          await failPromise,
+        const [[stages], ...checks] = await Promise.all([
+          await stagesPromise,
           ...keys.map((key) => redis.get(key).then((v) => Number(v ?? 0))),
         ]);
         return {
-          challenges: checks.length,
+          challenges: Number(stages?.challenges ?? 0),
           checks: checks.reduce((p, c) => p + c, 0),
-          fails: fails.reduce((p, c) => p + c.fails, 0),
+          fails: Number(stages?.fails ?? 0),
         };
       },
       listStages: (guild: string) => db.select().from(stagesTable).where(eq(stagesTable.guild, guild)),
